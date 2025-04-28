9e123a600236c97a0736751319fc5b3d2ba48348
DESKTOP-9IGA83T\Nizami Tech <abdullah3011liaquat@gmail.com>
2025-04-28 22:33:40 +0500

    changing due to enhancing with the respect of github

--- pages/all-products.vue
+++ pages/all-products.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class="page">
       <h1 class="heading">🛍️ Our Products</h1>
       <div class="product-grid">
@@ -6,16 +6,14 @@
       </div>
     </div>
   </template>
-  
   <script setup>
   import ProductCard from '~/components/ProductCard.vue'
-  
-  const products = ref([
+   const products = ref([
     {
       name: "Colorful Sneakers",
-      image: "https://github.com/user-attachments/assets/9d6fbfeb-0f26-4e15-af71-60195925dddb",
+      image: "https://images.pexels.com/photos/443446/pexels-photo-443446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       price: "$49.99",
-      description: "Stylish and comfortable sneakers for everyday use."
+      description: "Track your health and notifications on the go"
     },
     {
       name: "Smart Watch",
@@ -37,21 +35,43 @@
     }
   ])
   </script>
-  
   <style scoped>
-  .page {
-    padding: 2rem;
-    text-align: center;
-  }
-  .heading {
+   .page {
+     padding: 2rem;
+     text-align: center;
+   }
+   .heading {
     color: #333;
     font-size: 2.5rem;
-    margin-bottom: 30px;
-  }
-  .product-grid {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    gap: 20px;
-  }
-  </style>
+     margin-bottom: 30px;
+   }
+   .product-grid {
+     display: grid;
+     grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+     gap: 20px;
+   }
+   </style> -->
+   <template>
+    <div>
+   <nav class="navbar">
+    <div class="logo">My Store</div>
+    <ul class="nav-list">
+      <li class="nav-item"><NuxtLink to="/">Home</NuxtLink></li>
+      <li class="nav-item"><NuxtLink to="/">Products</NuxtLink></li>
+      <li class="nav-item"><NuxtLink to="/about">About Us</NuxtLink></li>
+      <li class="nav-item"><NuxtLink to="/contact">Contact</NuxtLink></li>
+    </ul>
+   
+  </nav>
+    </div>
+   </template>
+   
+   <script setup>
+   
+   </script>
+   
+   <style >
+  .navbar{
   
+  }
+   </style>
---
+++ pages/backstack.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+<figure v-for="(item, i) in images" :key="i">
+   <img  class="images" :src="(`/img/${item}.jpg`)">
+   </figure>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            images: ['Beach + Rock', 'dark Landscape', 'Night + Silence']
+            
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+.images {
+   width: 10%;
+   border-radius: 5px;
+   transition: transform 0.5s ease;
+   margin: 5px;
+   cursor: pointer;
+   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+   background-image: url('https://www.google.com/imgres?imgurl=https%3A%2F%2Fe0.pxfuel.com%2Fwallpapers%2F406%2F349%2Fdesktop-wallpaper-abstract-nature-mountain-abstract-thumbnail.jpg&tbnid=ZZdic3FgTyk4rM&vet=10CBQQxiAoBmoXChMIwOmlrNHujAMVAAAAAB0AAAAAEA8..i&imgrefurl=https%3A%2F%2Fwww.pxfuel.com%2Fen%2Fquery%3Fq%3Dnature&docid=M1v3VK96UaE71M&w=350&h=197&itg=1&q=wallpaper%20for%20pc&ved=0CBQQxiAoBmoXChMIwOmlrNHujAMVAAAAAB0AAAAAEA8');
+   &:hover {
+     transform: scale(1.55);
+     margin-bottom: 5px;
+
+   }
+}
+head{
+    background-color: #f2f2f2;
+    padding: 20px;
+    text-align: center;
+}
+</style>
